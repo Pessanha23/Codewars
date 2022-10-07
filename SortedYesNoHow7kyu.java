954399e3f16185ef8780bc0e0fca73469e1ccf66
João Ricardo Pagano Pessanha <joao.ricardopagano@gmail.com>
2022-10-07 13:47:00 -0300

    Codewars Sorted? yes? no? how? - 7kyu
     Exercise, for serching the order is ascending our descending, but when inside the vector there is no defined order, a string must be returned in the.
    
    Follow the link below for a better view:
    https://www.codewarshttps://www.codewars.com/kata/580a4734d6df748060000045/train/java

--- SortedYesNoHow7kyu.java
+++ SortedYesNoHow7kyu.java
@@ -3,5 +3,37 @@ import java.util.Scanner;
 public class SortedYesNoHow7kyu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int[] teste = {-50, -8, 10,25,35};
+
+        System.out.println(isSortedAndHow(teste));
+        sc.close();
+    }
+
+    public static String isSortedAndHow(int[] array) {
+        int menorA = Integer.MIN_VALUE;
+        int menorB = Integer.MIN_VALUE;
+        int maiorC = Integer.MAX_VALUE;
+        String result = "";
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] > menorA) {
+                menorB = menorA;
+                menorA = array[i];
+                result = "yes, ascending";
+            }
+           else if (array[i] < maiorC) {
+                maiorC = array[i];
+                result = "yes, descending";
+            }
+            if (maiorC < menorB && menorB < menorA) {
+                return "no";
+            }
+        }
+        return result;
+    }
+
+    public static void showResults(String value) {
+        System.out.println(value);
     }
 }
